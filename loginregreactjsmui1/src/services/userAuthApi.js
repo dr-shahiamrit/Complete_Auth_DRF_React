8760526d3b80224e21b0c6bea5318f044027316b
AMRIT SHAHI <141372453+dr-shahiamrit@users.noreply.github.com>
2024-02-29 00:30:42 +0545

    added

---
+++ loginregreactjsmui1/src/services/userAuthApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const userAuthApi = createApi({
+  reducerPath: 'userAuthApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/user/' }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (user) => ({
+        url: 'register/',
+        method: 'POST',
+        body: user,
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }),
+    }),
+  }),
+})
+
+export const { useRegisterUserMutation } = userAuthApi
